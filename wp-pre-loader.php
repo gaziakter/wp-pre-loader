034cd2628f164beb2377dcf8abecec8273d15de0
Gazi Akter <gaziakterdev@gmail.com>
2024-07-02 22:39:56 +0600

    Plugin head

---
+++ wp-pre-loader.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Plugin Name:       Wp Pre Loader
+ * Plugin URI:        https://gaziakter.com/plugins/wp-pre-loader/
+ * Description:       A WordPress Pre loader aplicaiton.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Gazi Akter
+ * Author URI:        https://gaziakter.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       pre-loader
+ * Domain Path:       /languages
+ */
+
+ if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
